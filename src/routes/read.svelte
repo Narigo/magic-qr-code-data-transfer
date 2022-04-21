f969c4797ca0d568fd2608e7c88de99597611162
Joern Bernhardt <joern.bernhardt@compose.us>
2022-04-22 00:11:43 +0200

    feat: Make the QR code scan video smaller on big screens
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/read.svelte
+++ src/routes/read.svelte
@@ -1,13 +1,25 @@
+<script lang="ts" context="module">
+	let id = 0;
+
+	function getNextId(prefix: string): string {
+		return `${prefix}${++id}`;
+	}
+</script>
+
 <script lang="ts">
 	import Button from '$lib/Button/Button.svelte';
 	import Headline from '$lib/Headline/Headline.svelte';
 	import PageWithNavigation from '$lib/PageWithNavigation/PageWithNavigation.svelte';
 	import Qrcode from '$lib/Qrcode/Qrcode.svelte';
 	import { Html5Qrcode } from 'html5-qrcode';
+	import type {
+		Html5QrcodeCameraScanConfig,
+		Html5QrcodeConfigs
+	} from 'html5-qrcode/esm/html5-qrcode';
 	import { onMount } from 'svelte';
 	import { writable } from 'svelte/store';
 
-	let state: 'ERROR' | 'SCANNING' | 'STOPPED' | 'RESULT' = 'STOPPED';
+	let state: 'ERROR' | 'INIT' | 'RESULT' | 'SCANNING' | 'STOPPED' = 'INIT';
 
 	let error: string = null;
 	let restoredData: string = '';
@@ -15,13 +27,15 @@
 
 	let html5QrcodeScanner: Html5Qrcode;
 	let scannedQrcodes = 0;
-	let totalQrcodesToScan = 1;
+	let totalQrcodesToScan = 0;
+
+	const readerId = getNextId('reader_');
 
 	onMount(() => {
-		html5QrcodeScanner = new Html5Qrcode('reader');
+		html5QrcodeScanner = new Html5Qrcode(readerId);
 	});
 
-	async function onStartClick(e) {
+	async function onStartClick() {
 		const qrCodeSuccessCallback = async (decodedText: string, decodedResult) => {
 			const prefixLength = `MQR00.00.`.length;
 			const prefix = decodedText.substring(0, prefixLength - 1);
@@ -54,10 +68,11 @@
 				restoredData = results.reduce((acc, [_prefix, data]) => acc + data, '');
 			}
 		};
-		const qrCodeErrorCallback = (error) => {
-			console.error(error);
+		const qrCodeErrorCallback = () => {};
+		const config: Html5QrcodeCameraScanConfig = {
+			fps: 250,
+			qrbox: (width, height) => ({ width: Math.min(width, 250), height: Math.min(height, 250) })
 		};
-		const config = { fps: 500, qrbox: { width: 250, height: 250 } };
 
 		try {
 			await html5QrcodeScanner.start(
@@ -88,7 +103,12 @@
 	}
 
 	async function onResetClick() {
+		await stopScanning();
 		$qrcodesData = {};
+		restoredData = '';
+		scannedQrcodes = 0;
+		totalQrcodesToScan = 0;
+		await onStartClick();
 	}
 
 	function nf(n: number): string {
@@ -97,44 +117,53 @@
 </script>
 
 <PageWithNavigation>
-	<div id="reader" />
+	<div id={readerId} class="reader" />
 
 	{#if state === 'ERROR'}
 		<Headline>Error scanning!</Headline>
 		<p>{error}</p>
 	{:else if state === 'SCANNING'}
-		<div class="progressBar">
-			<div class="progress" style={`width: ${(100 * scannedQrcodes) / totalQrcodesToScan}%;`} />
-		</div>
-		<div class="progressText">
-			{scannedQrcodes} of {totalQrcodesToScan} QR codes scanned.
-		</div>
+		{#if totalQrcodesToScan > 0}
+			<div class="progressBar">
+				<div class="progress" style={`width: ${(100 * scannedQrcodes) / totalQrcodesToScan}%;`} />
+			</div>
+			<div class="progressText">
+				{scannedQrcodes} of {totalQrcodesToScan} QR codes scanned.
+			</div>
 
-		<ul class="qrcodes">
-			{#each new Array(totalQrcodesToScan).fill(1).map((_, index) => {
-				return { index, chunk: $qrcodesData[`MQR${nf(index)}.${nf(totalQrcodesToScan - 1)}`] };
-			}) as element}
-				<li class="qrcode" class:scanned={!!element.chunk}>
-					{#if element.chunk}
-						<Qrcode value={element.chunk} />
-					{:else}
-						<Qrcode value="..." />
-					{/if}
-					<div>{element.index + 1}</div>
-				</li>
-			{/each}
-		</ul>
+			<ul class="qrcodes">
+				{#each new Array(totalQrcodesToScan).fill(1).map((_, index) => {
+					return { index, chunk: $qrcodesData[`MQR${nf(index)}.${nf(totalQrcodesToScan - 1)}`] };
+				}) as element}
+					<li class="qrcode" class:scanned={!!element.chunk}>
+						{#if element.chunk}
+							<Qrcode value={element.chunk} />
+						{:else}
+							<Qrcode value="..." />
+						{/if}
+						<div>{element.index + 1}</div>
+					</li>
+				{/each}
+			</ul>
+		{/if}
 		<Button on:click={onStopClick}>Stop scanning!</Button>
 	{:else if state === 'RESULT'}
 		<Headline>Successfully scanned all QR codes!</Headline>
 		<textarea bind:value={restoredData} />
+		<Button on:click={onResetClick}>Reset scanning</Button>
+	{:else if state === 'STOPPED'}
+		<Button on:click={onStartClick}>Continue scanning</Button>
+		<Button on:click={onResetClick}>Reset scanning</Button>
 	{:else}
 		<Button on:click={onStartClick}>Start scanning!</Button>
-		<Button on:click={onResetClick}>Reset scanning</Button>
 	{/if}
 </PageWithNavigation>
 
 <style>
+	.reader {
+		max-height: 500px;
+		max-width: 500px;
+	}
 	.progressBar {
 		background-color: rgba(50, 200, 50, 0.1);
 		height: 10px;
