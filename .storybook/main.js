8fc79371adc180fa371806bf09f6362eb1c73000
Joern Bernhardt <joern.bernhardt@compose.us>
2022-04-27 18:23:05 +0200

    chore: Fix multiple issues with storybook
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,7 @@ module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
 	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-svelte-csf'],
 	core: {
-		builder: 'storybook-builder-vite'
+		builder: '@storybook/builder-vite'
 	},
 	svelteOptions: {
 		preprocess: preprocess()
