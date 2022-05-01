c2377c54f10024e01f312001eb479801688b4e2a
Joern Bernhardt <joern.bernhardt@compose.us>
2022-05-01 15:38:49 +0200

    feat: Add QR code with link to read page
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

---
+++ src/lib/ShowQrcodeToReader/ShowQrcodeToReader.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+	import { page } from '$app/stores';
+	import Qrcode from '$lib/Qrcode/Qrcode.svelte';
+
+	const path = `${base}/read`;
+	const readerUrl = `${$page.url.origin}${path}`;
+</script>
+
+<figure>
+	<Qrcode value={readerUrl} />
+	<figcaption>You can use the above QR code to navigate to {readerUrl} on your phone.</figcaption>
+</figure>
+
+<style>
+	figure {
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
+		margin: 0;
+	}
+
+	figcaption {
+		margin: 0;
+		text-align: center;
+	}
+</style>
