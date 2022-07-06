4811852e80600ecb3b9dd73bcfdddc01a7ed8ca4
Joern Bernhardt <joern.bernhardt@compose.us>
2022-07-06 22:00:54 +0200

    chore: Add test for link to create page
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- integration/app.integration.ts
+++ integration/app.integration.ts
@@ -5,3 +5,9 @@ test('basic test', async ({ page }) => {
 	const headlines = await page.locator('h2').allTextContents();
 	await expect(headlines[0]).toMatch(/Magic QR Code/);
 });
+
+test('there is a link to the create page', async ({ page }) => {
+	await page.goto('http://localhost:3000/');
+	const links = await page.locator('a').allTextContents();
+	await expect(links.some((linkText) => /Send data/.test(linkText))).toBe(true);
+});
